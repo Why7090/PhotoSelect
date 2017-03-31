0052c3acf8ce3e781944ea14c76e19d036a7bdde
Why7090 <whyharry9@gmail.com>
2017-03-31 08:10:33 -0400

    添加项目文件。

---
+++ PhotoSelect/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace PhotoSelect
+{
+    partial class ImageSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageSelect));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveBookmarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewShortcutsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageBox1 = new Cyotek.Windows.Forms.ImageBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(160, 90);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(754, 25);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(230, 536);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemActivate += new System.EventHandler(this.OnImageSelected);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(984, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseFolderToolStripMenuItem,
+            this.saveBookmarksToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // browseFolderToolStripMenuItem
+            // 
+            this.browseFolderToolStripMenuItem.Name = "browseFolderToolStripMenuItem";
+            this.browseFolderToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.browseFolderToolStripMenuItem.Text = "Browse Folder";
+            this.browseFolderToolStripMenuItem.Click += new System.EventHandler(this.BrowseFolder);
+            // 
+            // saveBookmarksToolStripMenuItem
+            // 
+            this.saveBookmarksToolStripMenuItem.Name = "saveBookmarksToolStripMenuItem";
+            this.saveBookmarksToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.saveBookmarksToolStripMenuItem.Text = "Save bookmarks";
+            this.saveBookmarksToolStripMenuItem.Click += new System.EventHandler(this.Save);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewShortcutsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // viewShortcutsToolStripMenuItem
+            // 
+            this.viewShortcutsToolStripMenuItem.Name = "viewShortcutsToolStripMenuItem";
+            this.viewShortcutsToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.viewShortcutsToolStripMenuItem.Text = "View shortcuts";
+            this.viewShortcutsToolStripMenuItem.Click += new System.EventHandler(this.ShowHelp);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 25);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(754, 536);
+            this.imageBox1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(710, 25);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(44, 536);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::PhotoSelect.Properties.Resources.Star_32px;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.MinimumSize = new System.Drawing.Size(0, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 38);
+            this.button1.TabIndex = 5;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ToggleBookmark);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::PhotoSelect.Properties.Resources.Save_32px_1;
+            this.button2.Location = new System.Drawing.Point(3, 47);
+            this.button2.MinimumSize = new System.Drawing.Size(0, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 38);
+            this.button2.TabIndex = 6;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Save);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::PhotoSelect.Properties.Resources.Open_Folder_32px;
+            this.button3.Location = new System.Drawing.Point(3, 91);
+            this.button3.MinimumSize = new System.Drawing.Size(0, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 38);
+            this.button3.TabIndex = 7;
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.BrowseFolder);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::PhotoSelect.Properties.Resources.Delete_32px;
+            this.button4.Location = new System.Drawing.Point(3, 135);
+            this.button4.MinimumSize = new System.Drawing.Size(0, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 38);
+            this.button4.TabIndex = 8;
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.DeleteImage);
+            // 
+            // ImageSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ImageSelect";
+            this.Text = "PhotoSelect";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browseFolderToolStripMenuItem;
+        private Cyotek.Windows.Forms.ImageBox imageBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewShortcutsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveBookmarksToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ PhotoSelect/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Cyotek.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace PhotoSelect
+{
+    public partial class ImageSelect : Form
+    {
+        public FileInfo[] imagePath;
+        public Dictionary<string, Image> images = new Dictionary<string, Image>();
+        public List<int> bookmarks = new List<int>();
+
+        public ImageSelect ()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFolder (object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            imagePath = SearchImage(folderBrowserDialog1.SelectedPath);
+            images.Clear();
+            listView1.Items.Clear();
+            int length = imageList1.Images.Count;
+            for (int i = 0; i < length; i++)
+            {
+                var img = imageList1.Images[0];
+                imageList1.Images.RemoveAt(0);
+                img.Dispose();
+            }
+            foreach (var p in imagePath)
+            {
+                var image = Image.FromFile(p.FullName);
+                var name = p.Name;
+                images.Add(name, image);
+                imageList1.Images.Add(name, image);
+                var item = listView1.Items.Add(name, name);
+            }
+        }
+
+        private FileInfo[] SearchImage (string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+            var filter = new string[] { ".jpg", ".jpeg", ".png", ".gif", ".tiff", ".bmp" };
+            var files = folder.GetFiles("*.*", System.IO.SearchOption.TopDirectoryOnly)
+                .Where(f => filter.Contains(f.Extension.ToLower())).ToArray();
+
+            return files;
+        }
+
+        private void OnImageSelected (object sender, EventArgs e)
+        {
+            imageBox1.SizeMode = ImageBoxSizeMode.Stretch;
+            imageBox1.Image = images[listView1.SelectedItems[0].ImageKey];
+            imageBox1.SizeMode = ImageBoxSizeMode.Normal;
+            if (bookmarks.Contains(listView1.SelectedIndices[0]))
+            {
+                button1.Image = Properties.Resources.Star_Filled_32px;
+            }
+            else
+            {
+                button1.Image = Properties.Resources.Star_32px;
+            }
+        }
+
+        private void ToggleBookmark (object sender, EventArgs e)
+        {
+            listView1.Focus();
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var item = listView1.SelectedItems[0];
+                int index = item.Index;
+                if (bookmarks.Contains(index))
+                {
+                    bookmarks.Remove(index);
+                    item.BackColor = Color.Transparent;
+                    button1.Image = Properties.Resources.Star_32px;
+                }
+                else
+                {
+                    bookmarks.Add(index);
+                    item.BackColor = Color.Yellow;
+                    button1.Image = Properties.Resources.Star_Filled_32px;
+                }
+            }
+        }
+
+        private void Save (object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var path = folderBrowserDialog1.SelectedPath;
+            foreach (int index in bookmarks)
+            {
+                var file = imagePath[index];
+                file.CopyTo(Path.Combine(path, file.Name), false);
+            }
+            MessageBox.Show("Images saved to " + path);
+        }
+
+        private bool SelectImage (int relativeIndex)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedIndices[0] + relativeIndex;
+                if (index < listView1.Items.Count && index >= 0)
+                {
+                    listView1.Focus();
+                    listView1.Items[index].Selected = true;
+                    listView1.Items[index].Focused = true;
+                    OnImageSelected(null, null);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected override bool ProcessCmdKey (ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left || keyData == Keys.Up)
+            {
+                SelectImage(-1);
+                return true;
+            }
+            if (keyData == Keys.Right || keyData == Keys.Down)
+            {
+                SelectImage(1);
+                return true;
+            }
+            if (keyData == Keys.OemQuestion)
+            {
+                ToggleBookmark(null, null);
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void ShowHelp (object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "\"/\" : \n  Bookmark selected image\n\n" +
+                "Arrow left / up : \n  Select previous image\n\n" +
+                "Arrow right / down : \n  Select next image", "Shortcuts",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void DeleteImage (object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var item = listView1.SelectedItems[0];
+                if (MessageBox.Show("Move \"" + item.Text + "\" to recycle bin?", "Warning",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                {
+                    string key = item.ImageKey;
+                    int index = listView1.SelectedIndices[0];
+                    imageList1.Images[key].Dispose();
+                    imageList1.Images.RemoveByKey(key);
+                    images[key].Dispose();
+                    images.Remove(key);
+                    listView1.Items.Remove(item);
+                    try
+                    {
+                        FileSystem.DeleteFile(imagePath[index].FullName,
+                            UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Operation failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var lst = imagePath.ToList();
+                    lst.RemoveAt(index);
+                    imagePath = lst.ToArray();
+                }
+            }
+        }
+    }
+}
